9e173d45320fecd93e07fec60a084c516aa30342
jinwook-song <wlsdnr129@naver.com>
2021-12-21 17:44:17 +0900

    Styles with JSX

--- components/navigation.js
+++ components/navigation.js
@@ -1,30 +1,24 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./navigation.module.css";
 
 function Navigation() {
   const { pathname } = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a
-          className={`${styles.link} ${
-            pathname === "/" ? styles.active : null
-          }`}
-        >
-          Home
-        </a>
+        <a>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={[
-            styles.link,
-            pathname === "/about" ? styles.active : "",
-          ].join(" ")}
-        >
-          About
-        </a>
+        <a>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: teal;
+        }
+        a {
+          text-decoration: none;
+        }
+      `}</style>
     </nav>
   );
 }
